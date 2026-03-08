68d255564939ae5b874ffb473933847821494b0c
Mahalakshmi K <1476mahalakshmi@gmail.com>
2026-03-08 08:31:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def get_score():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
